ff8c1c733d7044497b4cc86612ee91511435b936
Adesh Srivastava <adeshsrivastava.ofc@gmail.com>
2026-01-19 04:03:01 +0530

    chore: initialize leetcode-java workspace

---
+++ src/main/java/com/adeshsrivastava/leetcode/LeetCodeRunner.java
@@ -0,0 +1,7 @@
+package com.adeshsrivastava.leetcode;
+
+public class LeetCodeRunner {
+    public static void main(String[] args) {
+        System.out.println("LeetCode Java workspace ready.");
+    }
+}
